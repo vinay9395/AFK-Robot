c28fa03605ba2ae35c6e0f79e52ba36f76b58ada
vinay9395 <163309318+vinay9395@users.noreply.github.com>
2024-06-13 17:53:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ from os import getenv
 from dotenv import load_dotenv
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
+API_ID = (getenv("API_ID")
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-LOG_ID = int(getenv("LOG_ID", ""))
+LOG_ID = ("-4220293329")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 SUDO_USER = list(map(int, getenv("SUDO_USER", "").split()))
