ad04f3f101d09138ec80de61598014c25604b7fa
vinay9395 <163309318+vinay9395@users.noreply.github.com>
2024-06-13 17:54:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import getenv
 from dotenv import load_dotenv
 load_dotenv()
 
-API_ID = (getenv("API_ID")
+API_ID = getenv("API_ID")
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
